83c04a8e5b04896ff738db0066425ac576fad6de
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-07-21 08:07:28 +0600

    Complite The home Page

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import catagories from "../data/catagories";
 
 const Categories = () =>
-  Object.keys(catagories).map((category) => (
-    <React.Fragment key={category}>
-      <ul>
-        <span className="pb-4  block text-[#5A7184] font-bold text-base">
-          {category}
-        </span>
-        {catagories[category].map((item) => (
-          <React.Fragment key={item}>
-            <li className="py-2 text-[#959EAD] text-sm font-normal">{item}</li>
-          </React.Fragment>
-        ))}
-      </ul>
-    </React.Fragment>
+  Object.keys(catagories).map((category, i) => (
+    <ul
+      key={category}
+      className={`col-span-5 pt-12 sm:pt-8 sm:col-span-4 lg:col-span-2 ${
+        i === 2 && " sm:col-start-5 "
+      }`}>
+      <span className="pb-4  block text-[#5A7184] font-bold text-base">
+        {category}
+      </span>
+      {catagories[category].map((item) => (
+        <React.Fragment key={item}>
+          <li className="py-2 text-[#959EAD] text-sm font-normal">{item}</li>
+        </React.Fragment>
+      ))}
+    </ul>
   ));
 export default Categories;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,11 @@ import Categories from "./Categories";
 
 const Footer = () => {
   return (
-    <footer className=" px-4 sm:px-20 lg:px-5 xl:px-0 bg-dark-hard">
-      <div className="container mx-auto">
-        <div className=" font-opensans flex flex-col-reverse sm:flex-row">
-          <div className="w-full flex flex-col justify-center items-center py-5 sm:py-0 sm:items-start sm:justify-start">
+    <footer className=" px-4 sm:px-20 lg:px-5 xl:px-0 sm:py-10 lg:py-20 bg-dark-hard">
+      <div className="container mx-auto  font-opensans ">
+        <div className="grid grid-cols-10 sm:grid-cols-12 ">
+          <Categories />
+          <div className="w-full flex flex-col justify-center items-center py-10 sm:items-start sm:justify-start col-span-10 sm:order-first sm:col-span-4 lg:col-span-4">
             <img src={images.wlogo} alt="logo" className="w-[90px] h-[28px]" />
             <p className="font-opensans text-[#5A7184]  py-2">
               Build a modern and creative website with crealand
@@ -26,9 +27,9 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="grid grid-cols-2 gap-5 justify-between w-full ">
-            <Categories />
-          </div>
+
+          {/* <div className="grid grid-cols-2 sm:grid-cols-4 gap-5 justify-between w-full ">
+          </div> */}
         </div>
       </div>
     </footer>
